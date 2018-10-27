6c471bd099cf2e27c8a2fd21c0027f67ce793997
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-27 15:14:38 +0200

    Set up variables and display random word from an array

---
+++ js/main.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', init);
+
+// Global variables
+let time = 5;
+let score = 0;
+let isPlaying;
+
+// DOM
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#senonds');
+
+const words = [
+    'promise',
+    'unnatural',
+    'tail',
+    'act',
+    'quirky',
+    'lackadaisical',
+    'type',
+    'embarrass',
+    'unused',
+    'crown',
+    'cloistered',
+    'grip',
+    'book',
+    'rinse',
+    'juicy',
+    'stain',
+    'paper',
+    'frighten',
+    'enchanted',
+    'ludicrous',
+];
+
+// Init
+function init() {
+    // Load a word from array
+    showWord(words);
+}
+
+// Pick and show random word
+function showWord(words) {
+    const randIndex = Math.floor(Math.random() * words.length);
+    currentWord.innerHTML = words[randIndex];
+}
